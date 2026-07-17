8760f7c4ca404972ad6b25868f3016aa1b6b627e
Engineer Mbole <tefumbole@yahoo.com>
2026-07-17 14:45:56 +0100

    Bump app version to 2.3.10 for production deploy.

--- config/app.php
+++ config/app.php
@@ -22,9 +22,9 @@ return [
     | Bump this on each release. Displayed on the login screen and footer.
     */
 
-    'version' => env('APP_VERSION', '2.3.9'),
+    'version' => env('APP_VERSION', '2.3.10'),
 
-    'version_label' => 'MGC V.' . env('APP_VERSION', '2.3.9'),
+    'version_label' => 'MGC V.' . env('APP_VERSION', '2.3.10'),
 
     /*
     |--------------------------------------------------------------------------
